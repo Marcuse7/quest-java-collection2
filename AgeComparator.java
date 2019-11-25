596c3793cadc82a6357d4c58a18450f2fc0a37fb
Markus Heinrichs <markus@heinrichs.es>
2019-11-25 14:32:49 +0100

    corrected age sort to descending

--- AgeComparator.java
+++ AgeComparator.java
@@ -4,8 +4,8 @@ public class AgeComparator implements Comparator<Hero> {
 	public int compare(Hero hero1, Hero hero2) {
 		if (hero1.getAge() == hero2.getAge()) {
 			return 0;
-		} else if (hero1.getAge() < hero2.getAge()) {
-			return -1; // hero1 younger than hero2
+		} else if (hero1.getAge() > hero2.getAge()) {
+			return -1; // hero1 older than hero2
 		} else {
 			return 1;
 		}
