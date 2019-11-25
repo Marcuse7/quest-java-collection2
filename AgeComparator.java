6d967bfaa60acf3d11dd4f879c646cba54dd0c46
Markus Heinrichs <markus@heinrichs.es>
2019-11-25 14:28:28 +0100

    Add sort by Hero name or age

---
+++ AgeComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class AgeComparator implements Comparator<Hero> {
+	public int compare(Hero hero1, Hero hero2) {
+		if (hero1.getAge() == hero2.getAge()) {
+			return 0;
+		} else if (hero1.getAge() < hero2.getAge()) {
+			return -1; // hero1 younger than hero2
+		} else {
+			return 1;
+		}
+	}
+}
